bd50d60f469db16a087e2da3dab0b41a399575a4
tanmayagarwal1 <tanmaytemp00010@gmail.com>
2021-08-16 15:54:59 +0530

    Proxy Pattern

---
+++ ProxyPattern/src/Pattern/Image.java
@@ -0,0 +1,6 @@
+package Pattern;
+
+public interface Image {
+	void display();
+
+}
---
+++ ProxyPattern/src/Pattern/MyMain.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Image img = new Proxyimage("Disk");
+		img.display();
+		System.out.println("");
+		img.display();
+
+	}
+
+}
